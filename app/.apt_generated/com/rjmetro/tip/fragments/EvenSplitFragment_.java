8f348b521e6ddc3dbe61527cadbe3977b84d842a
RJ Marsan <me@rj.io>
2012-10-20 03:39:16 -0500

    Adding lots of themeing

--- app/.apt_generated/com/rjmetro/tip/fragments/EvenSplitFragment_.java
+++ app/.apt_generated/com/rjmetro/tip/fragments/EvenSplitFragment_.java
@@ -28,11 +28,11 @@ public final class EvenSplitFragment_
     }
 
     private void afterSetContentView_() {
+        tipPercent = ((HalfHintEditText) findViewById(com.rjmetro.tip.R.id.tippercent_input));
         bill = ((HalfHintEditText) findViewById(com.rjmetro.tip.R.id.bill_input));
-        eachTipDollars = ((HalfHintEditText) findViewById(com.rjmetro.tip.R.id.each_tipdollars_input));
         eachTotal = ((HalfHintEditText) findViewById(com.rjmetro.tip.R.id.each_total_value));
         numberPeople = ((HalfHintEditText) findViewById(com.rjmetro.tip.R.id.people_input));
-        tipPercent = ((HalfHintEditText) findViewById(com.rjmetro.tip.R.id.tippercent_input));
+        eachTipDollars = ((HalfHintEditText) findViewById(com.rjmetro.tip.R.id.each_tipdollars_input));
         setup();
     }
 
